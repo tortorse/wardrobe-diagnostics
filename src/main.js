f5b351c1f05ac199b5e6249b2825a6d02fd7c533
tortorse <tortorse@gmail.com>
2018-09-03 17:35:24 +0800

    feat: Add google analytics

--- src/main.js
+++ src/main.js
@@ -3,12 +3,26 @@
 import Vue from 'vue'
 import Antd from 'vue-antd-ui'
 import 'vue-antd-ui/dist/antd.css'
+import VueAnalytics from 'vue-analytics'
 import App from './App'
 import router from './router'
 import 'ui-nuclear-mobile/dist/main.css'
 
 Vue.config.productionTip = false
 Vue.use(Antd)
+Vue.use(VueAnalytics, {
+  id: 'UA-97072659-1',
+  router,
+  autoTracking: {
+    pageviewTemplate (route) {
+      return {
+        title: route.name,
+        page: `/wardrobe-diagnostics/#${route.path}`,
+        location: window.location.href
+      }
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
